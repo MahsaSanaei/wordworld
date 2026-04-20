acff7d662315cb0e488d40033c4066865f687c86
MacBook <macbook@MacBooks-MacBook-Air.local>
2026-04-20 17:21:04 +0330

    Add WordGraph class  to init.py

--- wordword/__init__.py
+++ wordword/__init__.py
@@ -1,5 +1,6 @@
 from .inspector import inspect, count_syllables, classify_chars, is_palindrome
 from .corpus import Corpus
+from .relations import WordGraph
 
 __version__ = "0.1.0" 
 __author__ = "MAHSA"
