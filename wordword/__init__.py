dbdec4879fb89ca9877e266deeb2d2775ee7fbec
MacBook <macbook@MacBooks-MacBook-Air.local>
2026-05-09 10:53:39 +0330

    :sparkles: Add stage 6 functions to init

--- wordword/__init__.py
+++ wordword/__init__.py
@@ -2,6 +2,7 @@ from .inspector import inspect, count_syllables, classify_chars, is_palindrome
 from .corpus import Corpus
 from .relations import WordGraph
 from .personality import TextPersonality, score_personality
+from .visualizer import plot_word_frequency, plot_personality_radar, plot_sentence_length_distribution, compare_texts_visually
 
 
 __version__ = "0.1.0" 
