7409c00b9c4994366dcd1473ce7db62177a22f76
MacBook <macbook@MacBooks-MacBook-Air.local>
2026-07-20 11:33:28 +0330

    adding stage 5 functios to init

--- wordworld/__init__.py
+++ wordworld/__init__.py
@@ -2,6 +2,7 @@ from .inspector import inspect, count_syllables, classify_chars, is_palindrome
 from .corpus import Corpus
 from .relations import WordGraph
 from .personality import TextPersonality, score_personality
+from .timemachine import word_history, compare_words_history
 from .visualizer import plot_word_frequency, plot_personality_radar, plot_sentence_length_distribution, compare_texts_visually
 
 
